5a9bbc51b40cf418053742e08c9c712e2f64346c
Ryo Tanaka <dev@saku-lab.net>
2021-06-17 15:02:47 +0900

    add Athena table for CloudFront log (#33)
    * add Athena table for CloudFront log
    
    * make README.md

---
+++ aws/cloudfront-log-table/main.tf
@@ -0,0 +1,214 @@
+/**
+* ## Information
+*
+* Athena table for CloudFront log
+*
+* Notice: This module does not support partitioning.
+*
+* ### Usage
+*
+* ```hcl
+* module "main" {
+*   source = "github.com/elastic-infra/terraform-modules//aws/cloudfront-log-table?ref=v2.7.0"
+*
+*   name          = "main"
+*   database_name = "cflog"
+*   location      = "s3://cloudfront-log/main"
+* }
+* ```
+*
+*/
+
+resource "aws_glue_catalog_table" "t" {
+  name          = var.name
+  database_name = var.database_name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL = "TRUE"
+
+    # CloudFront log has two lines header
+    "skip.header.line.count" = 2
+  }
+
+  storage_descriptor {
+    location      = var.location
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim"          = "\t"
+        "serialization.format" = "\t"
+      }
+    }
+
+    # NOTE: https://docs.aws.amazon.com/athena/latest/ug/cloudfront-logs.html
+    columns {
+      name = "date"
+      type = "date"
+    }
+
+    columns {
+      name = "time"
+      type = "string"
+    }
+
+    columns {
+      name = "location"
+      type = "string"
+    }
+
+    columns {
+      name = "bytes"
+      type = "bigint"
+    }
+
+    columns {
+      name = "request_ip"
+      type = "string"
+    }
+
+    columns {
+      name = "method"
+      type = "string"
+    }
+
+    columns {
+      name = "host"
+      type = "string"
+    }
+
+    columns {
+      name = "uri"
+      type = "string"
+    }
+
+    columns {
+      name = "status"
+      type = "int"
+    }
+
+    columns {
+      name = "referrer"
+      type = "string"
+    }
+
+    columns {
+      name = "user_agent"
+      type = "string"
+    }
+
+    columns {
+      name = "query_string"
+      type = "string"
+    }
+
+    columns {
+      name = "cookie"
+      type = "string"
+    }
+
+    columns {
+      name = "result_type"
+      type = "string"
+    }
+
+    columns {
+      name = "request_id"
+      type = "string"
+    }
+
+    columns {
+      name = "host_header"
+      type = "string"
+    }
+
+    columns {
+      name = "request_protocol"
+      type = "string"
+    }
+
+    columns {
+      name = "request_bytes"
+      type = "bigint"
+    }
+
+    columns {
+      name = "time_taken"
+      type = "float"
+    }
+
+    columns {
+      name = "xforwarded_for"
+      type = "string"
+    }
+
+    columns {
+      name = "ssl_protocol"
+      type = "string"
+    }
+
+    columns {
+      name = "ssl_cipher"
+      type = "string"
+    }
+
+    columns {
+      name = "response_result_type"
+      type = "string"
+    }
+
+    columns {
+      name = "http_version"
+      type = "string"
+    }
+
+    columns {
+      name = "fle_status"
+      type = "string"
+    }
+
+    columns {
+      name = "fle_encrypted_fields"
+      type = "int"
+    }
+
+    columns {
+      name = "c_port"
+      type = "int"
+    }
+
+    columns {
+      name = "time_to_first_byte"
+      type = "float"
+    }
+
+    columns {
+      name = "x_edge_detailed_result_type"
+      type = "string"
+    }
+
+    columns {
+      name = "sc_content_type"
+      type = "string"
+    }
+
+    columns {
+      name = "sc_content_len"
+      type = "bigint"
+    }
+
+    columns {
+      name = "sc_range_start"
+      type = "bigint"
+    }
+
+    columns {
+      name = "sc_range_end"
+      type = "bigint"
+    }
+  }
+}
