93fc7cdebfc374d00588f8bdbfdb987cab0a2092
WATANABE Kyohei <k.watanabe@raksul.com>
2021-11-10 17:47:15 +0900

    use data instead of variable

--- aws/ei-privatelink-consumer/main.tf
+++ aws/ei-privatelink-consumer/main.tf
@@ -5,9 +5,11 @@ locals {
   }
 }
 
+data "aws_region" "current" {}
+
 resource "aws_vpc_endpoint" "this" {
   vpc_id              = var.vpc_id
-  service_name        = local.vpce_service_name[var.region]
+  service_name        = local.vpce_service_name[data.aws_region.current.name]
   vpc_endpoint_type   = "Interface"
   subnet_ids          = var.subnet_ids
   security_group_ids  = [aws_security_group.this.id]
