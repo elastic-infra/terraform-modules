d9f8f5c478d1bd249dfc2b01adbc46802f1af92a
Tomoya Kabe <limit.usus@gmail.com>
2022-02-22 06:26:27 +0900

    Set provider requirements to avoid warnings with newer provider

--- aws/redash/versions.tf
+++ aws/redash/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12.2, < 1.2.0"
+  required_version = ">= 0.13, < 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2"
+    }
+  }
 }
