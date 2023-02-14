dc288cbde112129dde68989a41e4eb2d3d2c2579
Hirotake Kobayashi <h.kobayashi@raksul.com>
2023-02-15 00:47:57 +0900

    Add cloudtrail log table (#67)

---
+++ aws/cloudtrail-table/main.tf
@@ -0,0 +1,172 @@
+/**
+* ## Information
+*
+* Athena table for CloudTrail log
+*
+* ### Usage
+*
+* ```hcl
+* module "main" {
+*   source = "github.com/elastic-infra/terraform-modules//aws/cloudtrail-table?ref=v5.1.0"
+*
+*   name          = "main"
+*   database_name = "cloudtrail"
+*   location      = "s3://CloudTrail_bucket_name/AWSLogs/<ACCOUNT-ID>/CloudTrail/<REGION>"
+* }
+* ```
+*
+*/
+
+resource "aws_glue_catalog_table" "t" {
+  name          = var.name
+  database_name = var.database_name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL = "TRUE"
+    # NOTE: https://docs.aws.amazon.com/athena/latest/ug/partition-projection.html
+    "projection.enabled"            = true
+    "projection.date.type"          = "date"
+    "projection.date.range"         = var.partition_range
+    "projection.date.format"        = "yyyy/MM/dd"
+    "projection.date.interval"      = 1
+    "projection.date.interval.unit" = "DAYS"
+    "storage.location.template"     = "${var.location}/$${date}"
+  }
+
+  storage_descriptor {
+    location      = var.location
+    input_format  = "com.amazon.emr.cloudtrail.CloudTrailInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hive.hcatalog.data.JsonSerDe"
+    }
+
+    columns {
+      name = "eventversion"
+      type = "string"
+    }
+
+    columns {
+      name = "useridentity"
+      type = "struct<type:string,principalId:string,arn:string,accountId:string,invokedBy:string,accessKeyId:string,userName:string,sessionContext:struct<attributes:struct<mfaAuthenticated:string,creationDate:string>,sessionIssuer:struct<type:string,principalId:string,arn:string,accountId:string,userName:string>,ec2RoleDelivery:string,webIdFederationData:map<string,string>>>"
+    }
+
+    columns {
+      name = "eventtime"
+      type = "string"
+    }
+
+    columns {
+      name = "eventsource"
+      type = "string"
+    }
+
+    columns {
+      name = "eventname"
+      type = "string"
+    }
+
+    columns {
+      name = "awsregion"
+      type = "string"
+    }
+
+    columns {
+      name = "sourceipaddress"
+      type = "string"
+    }
+
+    columns {
+      name = "useragent"
+      type = "string"
+    }
+
+    columns {
+      name = "errorcode"
+      type = "string"
+    }
+
+    columns {
+      name = "errormessage"
+      type = "string"
+    }
+
+    columns {
+      name = "requestparameters"
+      type = "string"
+    }
+
+    columns {
+      name = "responseelements"
+      type = "string"
+    }
+
+    columns {
+      name = "additionaleventdata"
+      type = "string"
+    }
+
+    columns {
+      name = "requestid"
+      type = "string"
+    }
+
+    columns {
+      name = "eventid"
+      type = "string"
+    }
+
+    columns {
+      name = "readonly"
+      type = "string"
+    }
+
+    columns {
+      name = "resources"
+      type = "array<struct<arn:string,accountId:string,type:string>>"
+    }
+
+    columns {
+      name = "eventtype"
+      type = "string"
+    }
+
+    columns {
+      name = "apiversion"
+      type = "string"
+    }
+
+    columns {
+      name = "recipientaccountid"
+      type = "string"
+    }
+
+    columns {
+      name = "serviceeventdetails"
+      type = "string"
+    }
+
+    columns {
+      name = "sharedeventid"
+      type = "string"
+    }
+
+    columns {
+      name = "vpcendpointid"
+      type = "string"
+    }
+
+    columns {
+      name = "tlsdetails"
+      type = "struct<tlsVersion:string,cipherSuite:string,clientProvidedHostHeader:string>"
+    }
+  }
+
+  partition_keys {
+    name = "date"
+    type = "string"
+  }
+}
