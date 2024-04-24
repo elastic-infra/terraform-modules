9201cd61fe1d29e4e470d6867bcef88a22b73954
Ryo Tanaka <dev@saku-lab.net>
2024-04-24 23:27:43 +0900

    add some outputs related to ECS (#85)

--- aws/redash/outputs.tf
+++ aws/redash/outputs.tf
@@ -12,3 +12,33 @@ output "lb_arn" {
   value       = aws_lb.redash.arn
   description = "The ARN of LB"
 }
+
+output "ecs_cluster_arn" {
+  value       = aws_ecs_cluster.redash.arn
+  description = "The ARN of ECS Cluster on which redash is running"
+}
+
+output "ecs_cluster_name" {
+  value       = aws_ecs_cluster.redash.name
+  description = "The Name of ECS Cluster on which redash is running"
+}
+
+output "ecs_service_server_arn" {
+  value       = aws_ecs_service.server.id
+  description = "The ARN of ECS Service on which server is running"
+}
+
+output "ecs_service_server_name" {
+  value       = aws_ecs_service.server.name
+  description = "The Name of ECS Service on which server is running"
+}
+
+output "ecs_service_worker_arn" {
+  value       = aws_ecs_service.worker.id
+  description = "The ARN of ECS Service on which worker is running"
+}
+
+output "ecs_service_worker_name" {
+  value       = aws_ecs_service.worker.name
+  description = "The Name of ECS Service on which worker is running"
+}
