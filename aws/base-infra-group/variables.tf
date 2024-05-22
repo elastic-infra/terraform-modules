9d04c421ca8d539b73b5314da4ca858d404f5696
yumaatt <yumaatt@gmail.com>
2024-05-22 09:27:51 +0000

    Minimize permissions

--- aws/base-infra-group/variables.tf
+++ aws/base-infra-group/variables.tf
@@ -1,11 +1,17 @@
-variable "reader_group_name" {
-  type        = string
-  default     = "iam-reader"
-  description = "Name of IAM reader group"
-}
-
 variable "system_group_name" {
   type        = string
   default     = "infra-system"
   description = "Name of system group"
+}
+
+variable "partition_ctl_s3_bucket" {
+  type        = list(string)
+  default     = []
+  description = "S3 bucket list for partition-ctl"
+}
+
+variable "use_register_r53" {
+  type        = bool
+  default     = false
+  description = "Whether register-r53 is used or not"
 }
